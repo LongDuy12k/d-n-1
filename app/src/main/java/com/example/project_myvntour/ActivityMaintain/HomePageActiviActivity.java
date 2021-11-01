5657685ce67eb2d7fab48dcf650770b26bd85b37
Đinh Thanh Minh <dinhthanhminhk11@gmail.com>
2021-11-01 11:28:31 +0700

    phần này là tester n

---
+++ app/src/main/java/com/example/project_myvntour/ActivityMaintain/HomePageActiviActivity.java
@@ -0,0 +1,16 @@
+package com.example.project_myvntour.ActivityMaintain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project_myvntour.R;
+
+public class HomePageActiviActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_activi);
+    }
+}
