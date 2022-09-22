e5eb99cdb9ceed94b44dcdd02e85a67e88c5d312
LongDuyDz <93507947+LongDuyDz@users.noreply.github.com>
2022-09-22 20:59:31 +0700

    dự án 1

--- app/src/main/java/com/example/project_myvntour/Database/Connect.java
+++ app/src/main/java/com/example/project_myvntour/Database/Connect.java
@@ -1,6 +1,7 @@
 package com.example.project_myvntour.Database;
 
 import android.os.StrictMode;
+import android.util.Log;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,13 +9,14 @@ import java.sql.DriverManager;
 public class Connect {
     Connection connection;
     public Connection getCollection(){
-        String ip = "192.168.21.104", port = "1433", user = "abc", pass = "123456", db = "MYVNTOUR";
+        String ip = "192.168.155.184", port = "1433", user = "sa", pass = "longduy", db = "MYVNTOUR";
         StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(threadPolicy);
         try {
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
             String connectUrl = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";databasename=" + db +";user=" + user +";password=" + pass +";";
             this.connection = DriverManager.getConnection(connectUrl);
+            Log.d("xxxxxxxx", "getCollection: ");
         } catch (Exception e) {
             e.printStackTrace();
         }
